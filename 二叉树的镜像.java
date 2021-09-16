241de4f03ff3f1e0eb7d184da3f188928c8d28bf
rekesai <546446335@qq.com>
2021-09-16 23:58:30 +0800

    test

---
+++ 二叉树的镜像.java
@@ -0,0 +1,64 @@
+package 树;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 二叉树的镜像 {
+
+
+    /**
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     * 例如输入：
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     *
+     * 镜像输出：
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     *
+     * 示例 1：
+     * 输入：root = [4,2,7,1,3,6,9]
+     * 输出：[4,7,2,9,6,3,1]
+     *
+     * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof
+     */
+    // 递归
+    public TreeNode mirrorTree1(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+    // 迭代
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            TreeNode tmp = cur.left;
+            cur.left = cur.right;
+            cur.right = tmp;
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+        return root;
+    }
+}
